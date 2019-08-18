d3ce280727b1b20a36fa32e34462c40da1663a06
Felipe Carlos Dos Santos <tusamal@Felipes-MacBook-Pro.local>
2019-08-18 10:36:58 -0300

    [upd] versão do angular e import de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomeModule' },
-  { path: 'feature', loadChildren: './feature/feature.module#FeatureModule' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'feature', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) },
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,24 +5,20 @@ import { AuthGuard } from 'guards/auth/auth.guard';
 import { HomePage } from './pages/home/home.page';
 import { HomeRootPage } from './home-root.page';
 
-const defaultRoutes: Routes = [
-  {
-    path: '',
-    component: HomeRootPage,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: '',
-        component: HomePage,
-        canActivate: [AuthGuard]
-      }
-    ]
-  }
-];
+const homeRoutes: Routes = [
+  { path: '', component: HomeRootPage, canActivate: [AuthGuard], children: [
+    { path: '', component: HomePage, canActivate: [AuthGuard] },
+    // { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+    // { path: ':id', component: RecipeDetailComponent },
+    // { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] },
+  ]},
+  // Módulos aninhados devem ser colocados fora para funcionar!
+  // { path: 'nested', loadChildren: './modules/feature-nested/feature-nested.module#FeatureNestedModule'},
+]
 
 @NgModule({
   imports: [
-    RouterModule.forChild(defaultRoutes)
+    RouterModule.forChild(homeRoutes)
   ],
   exports: [RouterModule],
   providers: [AuthGuard]
